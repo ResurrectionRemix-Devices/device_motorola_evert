80a0d3fb8f648b089a6862591523c9794b7c3b6d
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-01-22 22:10:25 -0500

    evert: RR :P
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- rr_evert.mk
+++ rr_evert.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common RR stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -68,7 +68,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := evert
-PRODUCT_NAME := lineage_evert
+PRODUCT_NAME := rr_evert
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G6 Plus
 PRODUCT_MANUFACTURER := Motorola
