0479ade1e0b051de08a03bbf48caaffc91c1fdf6
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-04-10 00:22:33 -0400

    evert: set props for addition calfiles
    * on stock there are two sets of acdbdata calib files,
      common and idn. let's use both (testing).
    
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -10,7 +10,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.calfile3=/vendor/etc/acdbdata/common/Handset_cal.acdb \
     persist.vendor.audio.calfile4=/vendor/etc/acdbdata/common/Hdmi_cal.acdb \
     persist.vendor.audio.calfile5=/vendor/etc/acdbdata/common/Headset_cal.acdb \
-    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/common/Speaker_cal.acdb
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/common/Speaker_cal.acdb \
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/idn/Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/idn/General_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/idn/Global_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/idn/Handset_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/idn/Hdmi_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/idn/Headset_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/idn/Speaker_cal.acdb \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
